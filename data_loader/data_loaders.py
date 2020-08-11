e7d7c681a4def51a0e968c887334f8af41ee2616
cnzero <zerolee18@gmail.com>
2020-08-11 14:53:09 +0800

    init code for Fashion_AlexNet explore

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,5 +1,6 @@
 from torchvision import datasets, transforms
 from base import BaseDataLoader
+import numpy as np
 
 
 class MNIST_DataLoader(BaseDataLoader):
@@ -13,4 +14,13 @@ class MNIST_DataLoader(BaseDataLoader):
         ])
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+class Fashion_DataLoader(BaseDataLoader):
+    # Fashion_MNIST dataset
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        # data/Fashion_MNIST/fashion-mnist_train.csv
+        # data/Fashion_MNIST/fashion-mnist_test.csv
+
+
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
--- model/Models.py
+++ model/Models.py
@@ -38,3 +38,52 @@ class MNIST_AlexNet(BaseModel):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+
+class Fashion_AlexNet(BaseModel):
+    def __init__(self, num_classes=10):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=96, kernel_size=11, stride=4, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(96, 256, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(256, 384, 3, 1, 1),
+            nn.ReLU()
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(384, 384, 3, 1, 1),
+            nn.ReLU()
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(384, 256, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2)
+        )
+
+        self.fc1 = nn.Linear(256 * 6 * 6, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, 10)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.conv5(out)
+        out = out.view(out.size(0), -1)
+
+        out = F.relu(self.fc1(out))  # 256*6*6 -> 4096
+        out = F.dropout(out, 0.5)
+        out = F.relu(self.fc2(out))
+        out = F.dropout(out, 0.5)
+        out = self.fc3(out)
+        out = F.log_softmax(out, dim=1)
+
+        return out
