597e703bd84ac8ce655ab5fb576b75f5c40989f6
cnzero <zerolee18@gmail.com>
2020-09-04 16:01:33 +0800

    target dimension should be (M,) instead of (M,1)

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,8 @@
 import torch.nn.functional as F
+import torch
 
 
 def nll_loss(output, target):
-    return F.nll_loss(output, target)
+    # return F.nll_loss(output, target)
+    return F.cross_entropy(output, target)
+    # return F.cross_entropy(output, target)
